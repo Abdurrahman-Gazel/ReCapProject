8c8b2f2da6d61b9bb7da11ab78e4c89aca038f18
Abdurrahman Gazel <173311078@ogr.selcuk.edu.tr>
2021-11-03 15:18:10 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            Console.WriteLine(carManager.GetCarById(2).Description);
+            Console.WriteLine("------------");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+
+        }
+    }
+}
